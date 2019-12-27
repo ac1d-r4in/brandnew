a1f91159a790fe18903e191c0f889d51ab51e8c6
429272-27 <429272-27>
2019-12-27 15:15:37 +0300

    K9

--- index.php
+++ index.php
@@ -1,36 +1,39 @@
 <?php
-include "core/LogAbstract.php";
-include "core/LogInterface.php";
-include "core/EquationInterface.php";
-include "Shablakov/MyException.php";
-include "Shablakov/Log.php";
-include "Shablakov/Linear.php";
-include "Shablakov/Square.php";
 
-use Shablakov\Log;
-use Shablakov\Square;
+define("BASEURI", __DIR__);
+date_default_timezone_set("Europe/Moscow");
+
+ini_set("display_errors", 1);
+error_reporting (-1); 
+
+use Shablakov\ShablakovException;
+use Shablakov\MyLog;
 use Shablakov\Linear;
-use Shablakov\MyException;
-/*
-include "core/loginterface.php";
-include "core/logabstract.php";
-include "core/equationinterface.php";
-include "Shablakov/log.php";
-include "Shablakov/myexception.php";
-include "Shablakov/linear.php";
-include "Shablakov/square.php";
-*/
-//работа 4
+use Shablakov\Square;
 
-echo "Enter koefs a, b, c \n";
 
-for($i = 0; $i < 3; $i++) {
-	fscanf(STDIN, "%d\n", $number);
-	$kfArray[] =  $number;
-}
+include BASEURI . '/core/EquationInterface.php';
+include BASEURI . '/core/LogInterface.php';
+include BASEURI . '/core/LogAbstract.php';
+include BASEURI . '/Shablakov/ShablakovException.php';
+include BASEURI . '/Shablakov/MyLog.php';
+include BASEURI . '/Shablakov/Linear.php';
+include BASEURI . '/Shablakov/Square.php';
 
-$equation = new Square();
-$equation->solve($kfArray[0], $kfArray[1], $kfArray[2]);
-Log::write();
 
-?>
+
+	try{
+		echo 'Enter a, b, c' . "\n";
+		$num = [];
+		for($i = 0; $i < 3; $i++){
+			$num[$i] = readline("Value = ");	
+		}
+		MyLog::log("The equation typed: " . $num[0] . "x^2 + " . $num[1] . "x + " . $num[2] . " = 0");
+		$square = new Square();
+		$roots = $square->solve($num[0], $num[1], $num[2]);
+		MyLog::log("Roots: " . implode(",", $roots) . "\n");
+	} catch(ShablakovException $e) {
+		MyLog::log($e->getMessage());
+	}
+
+MyLog::write();
